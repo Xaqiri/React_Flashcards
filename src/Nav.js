77c121494d59c98bdf898cbe8d45cc2789cbb925
Xaqiri <xaqiri@outlook.com>
2023-02-05 21:05:29 -0600

    Added redirect to SignUp and SignIn pages

--- src/Nav.js
+++ src/Nav.js
@@ -4,30 +4,32 @@ import {
     Route,
     Link
 } from 'react-router-dom'
-import './Nav.css'
-import SignUp from './pages/SignUp';
+import { useState } from 'react'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 
-const nav = () => {
-
+const Nav = () => {
+    const [user, setUser] = useState('')
     
     return (
         <Router>
-        <div id='nav-bar'>
-            <div id = 'nav-left'>
-                <Link to="/">Flashcards</Link>
+            <div id='nav-bar' className='p-4 flex justify-between bg-teal-600 text-white'>
+                <div id = 'nav-left'>
+                    <Link to="/" className='m-1 text-lg'>Flashcards</Link>
+                </div>
+                <div id = 'nav-right' className='w-1/6 flex justify-around'>
+                    <Link to="/sign-up" className='text-lg'>Sign Up</Link>
+                    <Link to="/sign-in" className='text-lg'>Sign In</Link>
+                </div>
             </div>
-            <div id = 'nav-right'>
-                <Link to="/sign-up">Sign Up</Link>
-                <Link to="/sign-in">Sign In</Link>
-            </div>
-        </div>
         <Routes>
+            <Route path="/" element={<Home user={user} />} />
             <Route path="/sign-up" exact element={<SignUp />} />
-            <Route path="/" exact element={<Home />} />
+            <Route path="/sign-in" exact element={<SignIn user={user} setUser={setUser}/>} />
         </Routes>
         </Router>
     );
 };
 
-export default nav;
+export default Nav;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import '../SignUp.css'
+import {React, useState} from 'react';
+import { useNavigate } from 'react-router';
+
 
 const SignUp = () => {
+    let [message, setMessage] = useState('Create a new user')
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        let user = {
+            name: e.target.userName.value,
+            password: e.target.userPass.value
+        }
+        let url = `http://localhost:8080/user/add`
+
+        await fetch(url, {
+            mode: 'cors',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },          
+            method: 'POST',
+            body: JSON.stringify(user)
+        })
+        .then(resp => {
+            return resp.text()
+        }).then(resp => {
+            e.target.userName.value = ''
+            e.target.userPass.value = ''
+            setMessage(resp)
+            alert('User saved')
+            if (resp === "Saved") navigate('/sign-in')
+        }).catch((e) => {
+            console.log(e)
+        })
+    }
+
     return (
-        <div id = 'sign-up-form'>
-            <div>Create a new user</div>
-            <form>
-                <input type='text'></input>
-                <input type='text'></input>
-                <button>Hello</button>
+        <div>
+            <div className='flex flex-col place-items-center'>
+            <h1 className='text-3xl font-bold'>{message}</h1>
+            <form onSubmit={handleSubmit} name='signInForm' className='flex flex-col w-1/2'>
+                <label className='flex justify-between'>Name: <input name='userName' type='text' className='border border-black m-1 w-1/2'></input></label>
+                <label className='flex justify-between'>Password: <input name='userPass' type='password' className='border border-black m-1 w-1/2'></input></label>
+                <button className=' place-self-center border border-black m-1 w-1/2'>Submit</button>
             </form>
+        </div>
         </div>
     )
 }
